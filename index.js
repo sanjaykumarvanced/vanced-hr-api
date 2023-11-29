84bf1ca49d192c85f1c01013dc52ac7f523c7f76
sanjaykumar@vancedsolutions.com <SanjayKumar@vancedsolutions.com>
2023-11-29 15:36:57 +0530

    Create 1701251318626-photo-1575936123452-b67c3203c357.jpg

--- index.js
+++ index.js
@@ -1,12 +1,15 @@
 const express = require('express');
 const cors = require('cors')
-const mongoose = require("mongoose");
-const routs = require("./api/router");
-const swaggerDocs = require("./api/swagger/swagger");
 const app = express()
+const routs = require("./api/router");
+const config = require("./config");
+let { PRODUCTION_PORT } = config;
+const PORT = PRODUCTION_PORT || 9000;
+const swaggerDocs = require("./api/swagger/swagger");
+const path = require('path');
+
 app.use(cors())
 app.use(express.json())
-const path = require('path');
 
 // Database connection
 require("./db/connection");
@@ -23,8 +26,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 // app.use(express.static("../public"));
 // app.use("/public", express.static("./public"));
 
-const PORT = process.env.PORT || 8800
-
 app.listen(PORT, () => {
   console.log("Server is running..." + PORT)
+  swaggerDocs(app, PORT);
 })
